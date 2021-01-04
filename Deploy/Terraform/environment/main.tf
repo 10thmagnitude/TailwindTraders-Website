57a9b008113aa7055dac911a766379945d945619
Colin Dembovsky <colindembovsky@gmail.com>
2021-01-04 11:11:19 -0600

    Add reserved=true to plan

--- Deploy/Terraform/environment/main.tf
+++ Deploy/Terraform/environment/main.tf
@@ -19,6 +19,7 @@ resource "azurerm_app_service_plan" "plan" {
   location                = data.azurerm_resource_group.main.location
   resource_group_name     = data.azurerm_resource_group.main.name
   kind                    = "Linux"
+  reserved                = true
 
   sku {
     tier                  = "Standard"
